9ccec8d8d1b6e14173a09dc2c8692be6a77a376b
Marston Connell <34043723+TheMarstonConnell@users.noreply.github.com>
2022-11-16 12:23:58 -0500

    refactored project structure

---
+++ jprovd/types/types.go
@@ -0,0 +1,62 @@
+package types
+
+import (
+	"sync"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	storagetypes "github.com/jackal-dao/canine/x/storage/types"
+)
+
+const MaxFileSize = 32 << 30
+
+type IndexResponse struct {
+	Status  string
+	Address string
+}
+
+type UploadResponse struct {
+	CID string
+	FID string
+}
+
+type ErrorResponse struct {
+	Error string
+}
+
+type VersionResponse struct {
+	Version string
+}
+
+type ListResponse struct {
+	Files []string
+}
+
+type QueueResponse struct {
+	Messages []sdk.Msg
+}
+
+type Message interface{}
+
+type Upload struct {
+	Message  sdk.Msg
+	Callback *sync.WaitGroup
+	Err      error
+	Response *sdk.TxResponse
+}
+
+type DataBlock struct {
+	Key   string
+	Value string
+}
+
+type DBResponse struct {
+	Data []DataBlock
+}
+
+type DealsResponse struct {
+	Deals []storagetypes.ActiveDeals
+}
+
+type StraysResponse struct {
+	Strays []storagetypes.Strays
+}
