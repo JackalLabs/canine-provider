62980ed30447bf49e8057895660e1d00352135a4
Marston Connell <34043723+TheMarstonConnell@users.noreply.github.com>
2023-02-06 15:07:46 -0500

    api improvements

--- jprov/api/data/dump.go
+++ jprov/api/data/dump.go
@@ -4,18 +4,21 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/JackalLabs/jackal-provider/jprov/api/types"
-	"github.com/julienschmidt/httprouter"
-	"github.com/spf13/cobra"
+	"github.com/JackalLabs/jackal-provider/jprov/utils"
 	"github.com/syndtr/goleveldb/leveldb"
 )
 
-func DumpDB(cmd *cobra.Command, w http.ResponseWriter, r *http.Request, ps httprouter.Params, db *leveldb.DB) {
+func DumpDB(w http.ResponseWriter, db *leveldb.DB) {
 	data := make([]types.DataBlock, 0)
 	iter := db.NewIterator(nil, nil)
 
 	for iter.Next() {
+		if string(iter.Key())[:4] == "TREE" {
+			continue
+		}
 		d := types.DataBlock{
 			Key:   string(iter.Key()),
 			Value: string(iter.Value()),
@@ -24,6 +27,61 @@ func DumpDB(cmd *cobra.Command, w http.ResponseWriter, r *http.Request, ps httpr
 	}
 
 	v := types.DBResponse{
+		Data: data,
+	}
+
+	err := json.NewEncoder(w).Encode(v)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func DumpDowntimes(w http.ResponseWriter, db *leveldb.DB) {
+	data := make([]types.DowntimeBlock, 0)
+	iter := db.NewIterator(nil, nil)
+
+	for iter.Next() {
+		if string(iter.Key())[:5] == utils.DOWNTIME_KEY {
+			i, err := strconv.Atoi(string(iter.Value()))
+			if err != nil {
+				continue
+			}
+			d := types.DowntimeBlock{
+				CID:      string(iter.Key())[5:],
+				Downtime: i,
+			}
+			data = append(data, d)
+
+		}
+	}
+
+	v := types.DowntimeResponse{
+		Data: data,
+	}
+
+	err := json.NewEncoder(w).Encode(v)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func DumpFids(w http.ResponseWriter, db *leveldb.DB) {
+	data := make([]types.FidBlock, 0)
+	iter := db.NewIterator(nil, nil)
+
+	for iter.Next() {
+		if string(iter.Key())[:5] == utils.FILE_KEY {
+
+			d := types.FidBlock{
+				CID: string(iter.Key())[5:],
+				FID: string(iter.Value()),
+			}
+			data = append(data, d)
+
+		}
+	}
+
+	v := types.FidResponse{
 		Data: data,
 	}
 
