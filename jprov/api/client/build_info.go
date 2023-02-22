a2e2240a3ca23c13abc794551ec3c97f84db20d7
Marston Connell <34043723+TheMarstonConnell@users.noreply.github.com>
2023-02-22 22:30:16 +0200

    lint

--- jprov/api/client/build_info.go
+++ jprov/api/client/build_info.go
@@ -16,16 +16,51 @@ func GetBuildInfo(cmd *cobra.Command, w http.ResponseWriter, r *http.Request, ps
 	_ = ps
 
 	port, err := cmd.Flags().GetInt(provTypes.FlagPort)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	version, err := cmd.Flags().GetString(provTypes.VersionFlag)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	noStrays, err := cmd.Flags().GetBool(provTypes.HaltStraysFlag)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	interval, err := cmd.Flags().GetUint16(provTypes.FlagInterval)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	threads, err := cmd.Flags().GetUint(provTypes.FlagThreads)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	maxMisses, err := cmd.Flags().GetInt(provTypes.FlagMaxMisses)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	chunkSize, err := cmd.Flags().GetInt64(provTypes.FlagChunkSize)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	strayInterval, err := cmd.Flags().GetInt64(provTypes.FlagStrayInterval)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	messageSize, err := cmd.Flags().GetInt(provTypes.FlagMessageSize)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 	gasProof, err := cmd.Flags().GetInt(provTypes.FlagGasCap)
-
 	if err != nil {
 		fmt.Println(err)
 		return
