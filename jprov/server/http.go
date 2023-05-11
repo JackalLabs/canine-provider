781eea0835b2195c694f569e6dbd2ea118388a5f
Marston Connell <34043723+TheMarstonConnell@users.noreply.github.com>
2023-05-10 22:00:16 -0400

    mem leak caused by ReadDir should be fixed, as ReadDir was removed

--- jprov/server/http.go
+++ jprov/server/http.go
@@ -77,31 +77,22 @@ func checkVersion(cmd *cobra.Command, w http.ResponseWriter, ctx *utils.Context)
 func downfil(cmd *cobra.Command, w http.ResponseWriter, ps httprouter.Params, ctx *utils.Context) {
 	clientCtx := client.GetClientContextFromCmd(cmd)
 
-	files, err := os.ReadDir(utils.GetStoragePath(clientCtx, ps.ByName("file")))
-	if err != nil {
-		ctx.Logger.Error(err.Error())
-		return
-	}
-
 	var data []byte
 
-	for i := 0; i < len(files); i += 1 {
-		f, err := os.ReadFile(filepath.Join(utils.GetStoragePath(clientCtx, ps.ByName("file")), fmt.Sprintf("%d.jkl", i)))
+	var i int
+	for { // loop through every file in the directory and fail once it hits a file that it can't find
+		path := filepath.Join(utils.GetStoragePath(clientCtx, ps.ByName("file")), fmt.Sprintf("%d.jkl", i))
+		f, err := os.ReadFile(path)
 		if err != nil {
-			ctx.Logger.Info("Error can't open file!")
-			_, err = w.Write([]byte("cannot find file"))
-			if err != nil {
-				ctx.Logger.Error(err.Error())
-			}
-			return
+			break
 		}
-
 		data = append(data, f...)
+		i++
 	}
 
 	w.Header().Set("Content-Length", fmt.Sprintf("%d", len(data)))
 
-	_, err = w.Write(data)
+	_, err := w.Write(data)
 	if err != nil {
 		return
 	}
