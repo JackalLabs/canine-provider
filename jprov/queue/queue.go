6e26294c4aedf3ddfbb07e038779c9d27d8d0aa4
Daniel Ahn <dahn510@gmail.com>
2023-05-30 18:02:39 -0700

    add prepareMessages() and updateQueue()

--- jprov/queue/queue.go
+++ jprov/queue/queue.go
@@ -1,6 +1,7 @@
 package queue
 
 import (
+	"errors"
 	"fmt"
 	"time"
 
@@ -33,6 +34,58 @@ func (q *UploadQueue) Append(upload *types.Upload) {
 	}
 
 	q.Queue = append(q.Queue, upload)
+}
+
+// Return a list of messages of the upload queue up to maxMessageSize in FCFS order.
+// Returns nil in these conditions:
+//  1. maxMessageSize is too small
+//  2. the UploadQueue is locked
+//  3. the upload queue is empty
+func (q *UploadQueue) prepareMessage(maxMessageSize int) (messages []cosmosTypes.Msg) {
+	if maxMessageSize < 1 || q.Locked || len(q.Queue) == 0 {
+		return nil
+	}
+
+	var netMsgSize int
+	for _, q := range q.Queue {
+		msgSize := len(q.Message.String())
+
+		if netMsgSize+msgSize > maxMessageSize {
+			break
+		} else {
+			netMsgSize += msgSize
+			messages = append(messages, q.Message)
+		}
+	}
+
+	return
+}
+
+// Update the upload queue with the parameter fields of count
+func (q *UploadQueue) updateQueue(count int, err error, res *cosmosTypes.TxResponse) {
+	if q.Locked || len(q.Queue) == 0 || len(q.Queue) < count {
+		return
+	}
+
+	for i := 0; i < count; i++ {
+		q := q.Queue[i]
+
+		if err != nil {
+			q.Err = err
+		} else {
+			if res != nil {
+				if res.Code != 0 {
+					q.Err = errors.New(res.RawLog)
+				} else {
+					q.Response = res
+				}
+			}
+		}
+
+		if q.Callback != nil {
+			q.Callback.Done()
+		}
+	}
 }
 
 func (q *UploadQueue) listenOnce(cmd *cobra.Command, providerName string) {
