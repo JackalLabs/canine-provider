93cb01fcd35ab227f0afe12f032f0455d662e5eb
Daniel Ahn <dahn510@gmail.com>
2023-05-31 14:45:16 -0700

    update listenOnce()

--- jprov/queue/queue.go
+++ jprov/queue/queue.go
@@ -99,9 +99,7 @@ func (q *UploadQueue) listenOnce(cmd *cobra.Command, providerName string) {
 
 	ctx := utils.GetServerContextFromCmd(cmd)
 
-	l := len(q.Queue)
-
-	if l == 0 {
+	if len(q.Queue) == 0 {
 		return
 	}
 
@@ -110,56 +108,16 @@ func (q *UploadQueue) listenOnce(cmd *cobra.Command, providerName string) {
 		ctx.Logger.Error(err.Error())
 	}
 
-	var totalSizeOfMsgs int
-	msgs := make([]cosmosTypes.Msg, 0)
-	uploads := make([]*types.Upload, 0)
-
-	for i := 0; i < l; i++ { // loop through entire queue
-
-		upload := q.Queue[i]
-
-		uploadSize := len(upload.Message.String())
-
-		// if the size of the upload would put us past our cap, we cut off the queue and send only what fits
-		if totalSizeOfMsgs+uploadSize > maxSize {
-			msgs = msgs[:len(msgs)-1]
-			uploads = uploads[:len(uploads)-1]
-			l = i
-
-			break
-		} else {
-			uploads = append(uploads, upload)
-			msgs = append(msgs, upload.Message)
-			totalSizeOfMsgs += len(upload.Message.String())
-		}
-
-	}
+	msgs := q.prepareMessage(maxSize)
 
 	clientCtx := client.GetClientContextFromCmd(cmd)
 	ctx.Logger.Debug(fmt.Sprintf("total no. of msgs in proof transaction is: %d", len(msgs)))
 
 	res, err := utils.SendTx(clientCtx, cmd.Flags(), fmt.Sprintf("Storage Provided by %s", providerName), msgs...)
-	for _, v := range uploads {
-		if v == nil {
-			continue
-		}
-		if err != nil {
-			v.Err = err
-		} else {
-			if res != nil {
-				if res.Code != 0 {
-					v.Err = fmt.Errorf(res.RawLog)
-				} else {
-					v.Response = res
-				}
-			}
-		}
-		if v.Callback != nil {
-			v.Callback.Done()
-		}
-	}
 
-	q.Queue = q.Queue[l:] // pop every upload that fit off the queue
+	q.updateQueue(len(msgs), err, res)
+
+	q.Queue = q.Queue[len(msgs):] // pop every upload that fit off the queue
 }
 
 func (q *UploadQueue) StartListener(cmd *cobra.Command, providerName string) {
