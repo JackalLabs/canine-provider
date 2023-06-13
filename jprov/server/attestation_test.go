733672edae4c20540fbb0e9bfa7b7272f4166b0c
Daniel Ahn <dahn510@gmail.com>
2023-06-12 17:38:23 -0700

    update TestVerifyAttest to use testutil tools

--- jprov/server/attestation_test.go
+++ jprov/server/attestation_test.go
@@ -1,20 +1,13 @@
 package server_test
 
 import (
-	"crypto/sha256"
-	"encoding/hex"
-	"encoding/json"
-	"fmt"
-	"io"
 	"testing"
 
 	"github.com/JackalLabs/jackal-provider/jprov/queue"
 	"github.com/JackalLabs/jackal-provider/jprov/server"
 	"github.com/JackalLabs/jackal-provider/jprov/types"
-	storagetypes "github.com/jackalLabs/canine-chain/x/storage/types"
+	"github.com/JackalLabs/jackal-provider/jprov/testutils"
 	"github.com/stretchr/testify/require"
-	"github.com/wealdtech/go-merkletree"
-	"github.com/wealdtech/go-merkletree/sha3"
 )
 
 func TestVerifyAttest(t *testing.T) {
@@ -35,37 +28,11 @@ func TestVerifyAttest(t *testing.T) {
 
 	for name, c := range cases {
 		t.Run(name, func(t *testing.T) {
-			var data [][]byte
-			h := sha256.New()
-			_, err := io.WriteString(h, fmt.Sprintf("%d%x", 0, "hello world"))
-			if err != nil {
-				t.Error(err)
-			}
+			testFile := testutils.NewFile([]byte("hello world"))
 
-			data = append(data, h.Sum(nil))
+			c.attest.HashList = string(testFile.GetJsonProof())
 
-			tree, err := merkletree.NewUsing(data, sha3.New512(), false)
-			if err != nil {
-				t.Error(err)
-			}
-
-			proof, err := tree.GenerateProof(data[0], 0)
-			if err != nil {
-				t.Error(err)
-			}
-
-			jproof, err := json.Marshal(*proof)
-			if err != nil {
-				t.Error(err)
-			}
-			c.attest.HashList = string(jproof)
-
-			activeDeal := storagetypes.ActiveDeals{
-				Blocktoprove: "0",
-				Merkle:       hex.EncodeToString(tree.Root()),
-			}
-
-			v, e := server.VerifyAttest(activeDeal, c.attest)
+			v, e := server.VerifyAttest(testFile.GenerateActiveDeal(), c.attest)
 
 			if c.verified != v {
 				t.Log("expected: ", c.verified, " got: ", v)
