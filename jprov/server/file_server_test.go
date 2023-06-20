d758c33cb4fe1c436346c931cb7c418033572aed
Daniel Ahn <dahn510@gmail.com>
2023-06-19 19:38:12 -0700

    add unit test setup for BuildCid()

--- jprov/server/file_server_test.go
+++ jprov/server/file_server_test.go
@@ -1,14 +1,16 @@
 package server_test
 
 import (
+	"crypto/sha256"
 	"encoding/json"
 	"errors"
 	"net/http/httptest"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/JackalLabs/jackal-provider/jprov/server"
 	"github.com/JackalLabs/jackal-provider/jprov/types"
+	"github.com/JackalLabs/jackal-provider/jprov/utils"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestWriteResponse(t *testing.T) {
@@ -49,7 +51,48 @@ func TestWriteResponse(t *testing.T) {
 			}
 			
 			assert.NotNil(t, rec.Body)
+			// converted to string for easier reading
 			assert.Equal(t, string(expResult)+"\n", rec.Body.String())	
+		})
+	}
+}
+
+func TestBuildCid(t *testing.T) {
+	cases := map[string]struct{
+		address string
+		sender string
+		fid string
+		expErr bool
+	}{
+		"valid_cid": {
+			address: "example_address",
+			sender: "example_sender",
+			fid: "example_fid",
+			expErr: false,
+		},
+	}
+
+	for name, c := range cases {
+		t.Run(name, func(t *testing.T){
+			cid, err := server.BuildCid(c.address, c.sender, c.fid)
+
+			if c.expErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+			
+			footprint := c.sender + c.address + c.fid
+
+			h := sha256.New()
+			_, err = h.Write([]byte(footprint))
+			if err != nil {
+				t.Error(err)
+				t.FailNow()
+			}
+			expCid, _ := utils.MakeCid(h.Sum(nil))
+
+			assert.Equal(t, expCid, cid)
 		})
 	}
 }
