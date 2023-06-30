e3aa01678e6cfc74abc7d78e8c602cc0f2e8b738
Daniel Ahn <dahn510@gmail.com>
2023-06-30 16:12:46 -0700

    add GetProofFilePath() to MerkleArchive

--- jprov/archive/merkle.go
+++ jprov/archive/merkle.go
@@ -1,10 +1,29 @@
 package archive
 
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/cosmos/cosmos-sdk/client"
+)
+
 var _ ProofArchive = &MerkleProofArchive{}// Compile time check
 
-type MerkleProofArchive struct {}
+type MerkleProofArchive struct {
+	baseDir string
+}
+
+func NewMerkleProofArchive (ctx client.Context) MerkleProofArchive {
+	return MerkleProofArchive{baseDir: ctx.HomeDir}
+}
 
 func (m *MerkleProofArchive) Save(id []byte, p proof) error {
+	f, err := os.OpenFile(m.GetProofFilePath(id), os.O_WRONLY|os.O_CREATE, 0o666)
+	defer f.Close()
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
@@ -14,4 +33,8 @@ func (m *MerkleProofArchive) Delete(id []byte) error {
 
 func (m *MerkleProofArchive) Retrieve(id []byte) (proof, error) {
 	return nil, nil
+}
+
+func (m *MerkleProofArchive) GetProofFilePath(id []byte) string {
+	return filepath.Join(m.baseDir, fmt.Sprintf("%s.tree", string(id)))
 }
