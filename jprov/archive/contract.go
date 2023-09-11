aa34361e3390b25959439a7c056e1bbb4a1175b3
Daniel Ahn <dahn510@gmail.com>
2023-09-11 13:59:17 -0700

    ArchiveDB implementation

--- jprov/archive/contract.go
+++ jprov/archive/contract.go
@@ -11,10 +11,10 @@ import (
 )
 
 type ArchiveDB interface {
-	GetFid(cid types.Cid) (fid types.Fid, found bool)
-	GetContracts(fid types.Fid) (cid []types.Cid)
+	GetFid(cid types.Cid) (types.Fid, error)
+	GetContracts(fid types.Fid) ([]types.Cid, error)
 	SetContract(cid types.Cid, fid types.Fid) error
-	DeleteContract(cid types.Cid)
+	DeleteContract(cid types.Cid) error
 	NewIterator() iterator.Iterator
 	Close() error
 } 
@@ -44,69 +44,115 @@ func NewDoubleRefArchiveDB (filepath string) (*DoubleRefArchiveDB, error) {
 	return &DoubleRefArchiveDB{db: db}, nil
 }
 
-func (d *DoubleRefArchiveDB) GetFid(cid types.Cid) (fid types.Fid, found bool) {
-	value, err := d.db.Get(d.Key(cid), nil)
+func (d *DoubleRefArchiveDB) GetFid(cid types.Cid) (types.Fid, error) {
+	value, err := d.db.Get(d.key(cid), nil)
 	if err != nil {
-		return "", false
+		return "", err
 	}
-	return types.Fid(value), true
+	return types.Fid(value), err
 }
-func (d *DoubleRefArchiveDB) GetContracts(fid types.Fid) (cid []types.Cid){
+
+func (d *DoubleRefArchiveDB) GetContracts(fid types.Fid) ([]types.Cid, error){
 	value, err := d.db.Get([]byte(fid), nil)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
+	var cid []types.Cid
 	cids := strings.Split(string(value), cidSeparator)
 	for _, c := range cids {
 		cid = append(cid, types.Cid(c))
 	}
-	return 
+	return cid, nil
 }
+
 func (d *DoubleRefArchiveDB) SetContract(cid types.Cid, fid types.Fid) error {
 	value, err := d.db.Get([]byte(cid), nil)
+	if err != nil {
+		return err
+	}
 	if value != nil {
 		return errors.New("already exist")
 	}
 
-	err = d.db.Put([]byte(cid), []byte(fid), nil)
+	batch := new(leveldb.Batch)
+	batch.Put([]byte(cid), []byte(fid))
+
+	err = d.addReference(batch, cid, fid)
 	if err != nil {
 		return err
 	}
-
-	err = d.setReference(cid, fid)
+	err = d.db.Write(batch, nil)
 	return err
 }
 
-func (d *DoubleRefArchiveDB) setReference(cid types.Cid, fid types.Fid) error {
+func (d *DoubleRefArchiveDB) addReference(batch *leveldb.Batch, cid types.Cid, fid types.Fid) error {
 	value, err := d.db.Get([]byte(cid), nil)
 	if err == leveldb.ErrNotFound {
 		value = nil
-	} else {
+	} else if err != nil {
 		return err
 	}
 
-	build := strings.Builder{}
-	_, _ = build.WriteString(string(value))
-	_, _ = build.WriteString(cidSeparator)
-	_, _ = build.WriteString(string(cid))
+	// reference look like this "potato,tomato,...,onion,"
+	var b strings.Builder
+	_, _ = b.WriteString(string(value))
+	_, _ = b.WriteString(string(cid))
+	_, _ = b.WriteString(cidSeparator)
 
-	err = d.db.Put([]byte(fid), []byte(build.String()), nil)
+	batch.Put([]byte(fid), []byte(b.String()))
+	return nil
+}
+
+func (d *DoubleRefArchiveDB) DeleteContract(cid types.Cid) error {
+	batch := new(leveldb.Batch)
+	err := d.deleteReference(batch, cid)
+	if err != nil {
+		return err
+	}
+
+	batch.Delete([]byte(cid))
+	err = d.db.Write(batch, nil)
 	return err
 }
 
-func (d *DoubleRefArchiveDB) DeleteContract(cid types.Cid){
-	err := d.db.Delete([]byte(cid), nil)
+func (d *DoubleRefArchiveDB) deleteReference (batch *leveldb.Batch, cid types.Cid) error {
+	fid, err := d.db.Get([]byte(cid), nil)
 	if err != nil {
-		panic(err)
+		return err
 	}
+
+	cidList, err := d.db.Get(fid, nil)
+	if err != nil {
+		return err
+	}
+	
+	var b strings.Builder
+	b.WriteString(string(cid))
+	b.WriteString(cidSeparator)
+	
+	result := strings.Replace(string(cidList), b.String(), "", 1)
+
+	if len(result) == 0 {
+		batch.Delete(fid)
+	} else {
+		batch.Put(fid, []byte(result))
+	}
+
+	return nil
 }
+
 func (d *DoubleRefArchiveDB) NewIterator() iterator.Iterator{
 	return d.db.NewIterator(nil, nil)
 }
+
 func (d *DoubleRefArchiveDB) Close() error{
 	return d.db.Close()
 }
 
-func (d *DoubleRefArchiveDB) Key(cid types.Cid) (key []byte) {
+func (d *DoubleRefArchiveDB) key(cid types.Cid) (key []byte) {
 	return []byte(cid)
+}
+
+func (d *DoubleRefArchiveDB) refKey(fid types.Fid) []byte {
+	return []byte(fid)
 }
