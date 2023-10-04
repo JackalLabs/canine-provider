7db95cfa8f6701b7de645d3d662e89b9ccac0ac4
Daniel Ahn <dahn510@gmail.com>
2023-10-04 13:11:36 -0700

    fix archive GetPiece unit test

--- jprov/archive/archive_test.go
+++ jprov/archive/archive_test.go
@@ -1,8 +1,9 @@
 package archive_test
 
 import (
-    "testing"
-    "os"
+    "bytes"
+	"os"
+	"testing"
 
 	"github.com/JackalLabs/jackal-provider/jprov/archive"
 )
@@ -16,24 +17,27 @@ func newFile(name string, t *testing.T) (f *os.File) {
 }
 
 func TestGetPiece(t *testing.T) {
-	file := newFile("TestGetPiece", t)
-	defer os.Remove(file.Name())
-	defer file.Close()
+    buf := bytes.NewBuffer(nil)
+    _, err := buf.WriteString("hello, world\n")
+    if err != nil {
+        t.Fatal(err)
+    }
 
     archive := archive.NewSingleCellArchive("")
 
-	const data = "hello, world\n"
-	_, err := file.WriteString(data)
-	if err != nil {
-		t.Fatalf("WriteString: %s", err)
-	}
-
-    _, err = archive.WriteFileToDisk(file, file.Name())
+    fileName := "testfile"
+    _, err = archive.WriteFileToDisk(buf, fileName)
     if err != nil {
         t.Fatalf("archive.WriteFileToDisk: %s", err)
     }
+    defer func(){
+        if err := os.RemoveAll("storage"); err != nil {
+            t.Fatal(err)
+            return
+        }
+    }()
 
-	resData, resErr := archive.GetPiece(file.Name(), 0, 5)
+	resData, resErr := archive.GetPiece(fileName, 0, 5)
 	if err != nil {
 		t.Errorf("GetPiece 0, 5: %s", resErr)
 	}
@@ -41,7 +45,7 @@ func TestGetPiece(t *testing.T) {
 		t.Errorf("GetPiece 0, 5: have %q, want %q", string(resData), "hello")
 	}
 
-	resData, resErr = archive.GetPiece(file.Name(), 1, 5)
+	resData, resErr = archive.GetPiece(fileName, 1, 5)
 	if err != nil {
 		t.Errorf("GetPiece 1, 5: %s", resErr)
 	}
@@ -50,11 +54,12 @@ func TestGetPiece(t *testing.T) {
 	}
 
 	// Test reading a block that starts almost at the end
-	resData, resErr = archive.GetPiece(file.Name(), 1, 8)
+	resData, resErr = archive.GetPiece(fileName, 1, 8)
 	if err != nil {
 		t.Errorf("GetPiece 1, 8: %s", resErr)
 	}
 	if string(resData) != "orld\n\x00\x00\x00" {
 		t.Errorf("GetPiece 1, 8: have %q, want %q", string(resData), "world")
 	}
+
 }
