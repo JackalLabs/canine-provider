e42b2cad7a66cb6b67994da6de8b6ea0845cd2a1
Daniel Ahn <dahn510@gmail.com>
2023-11-08 15:12:19 -0800

    split contract clean up with posting proofs The idea is to clean stale files first then post proofs.

--- jprov/server/query.go
+++ jprov/server/query.go
@@ -1,16 +1,39 @@
 package server
 
 import (
+    "strconv"
     "google.golang.org/grpc/status"
     "google.golang.org/grpc/codes"
 	storageTypes "github.com/jackalLabs/canine-chain/x/storage/types"
 )
 
-func (f *FileServer) isThatMine(cid string) (bool) {
+const (
+    verified = "verified"
+    notVerified = "not verified"
+    notFound = "not found"
+)
+
+// returns verified, notVerified or notFound
+func (f *FileServer) QueryContractState(cid string) string {
     req := storageTypes.QueryActiveDealRequest{Cid: cid}
     resp, err := f.queryClient.ActiveDeals(f.cmd.Context(), &req)
-    if stat, ok := status.FromError(err); ok && stat.Code() == codes.NotFound {
-        return false
+
+    stat, ok := status.FromError(err)
+    isDealMine := resp.ActiveDeals.Provider == f.provider.Address
+    if (ok && stat.Code() == codes.NotFound) || !isDealMine {
+        return notFound
     }
-    return resp.ActiveDeals.Provider == f.provider.Address
+
+    v, err := strconv.ParseBool(resp.ActiveDeals.Proofverified)
+    if err != nil {
+        // if this happens then api must have changed in chain
+        f.logger.Error("failed to parse ActiveDeals.Proofverified query resp")
+        return notFound
+    }
+
+    if v {
+        return verified
+    }
+
+    return notVerified
 }
